c03fdbce608a630f66236841abff194875bc6e1c
anilgadekar2805 <anilgadekar2268@gmail.com>
2021-02-08 13:06:01 +0530

    project setup and adding dependencies into projects

---
+++ smart_contact_manager/src/test/java/com/scma/anilg/SmartContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scma.anilg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
